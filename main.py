25371028d475062f143e93dae4c71d2a2e38b6ee
seikacu <neohack@xakep.ru>
2023-07-01 01:09:56 +0300

    upload changes

--- main.py
+++ main.py
@@ -2,27 +2,24 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-from fake_useragent import UserAgent
-import time
 from auth_data import bank_password, bank_emale
 
-useragent = UserAgent()
+import time
 
-# options
 options = webdriver.ChromeOptions()
 
-# user-agent
-options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 YaBrowser/23.5.2.625 Yowser/2.5 Safari/537.36")
+def set_driver_options(options:webdriver.ChromeOptions):
+    # user-agent
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 YaBrowser/23.5.2.625 Yowser/2.5 Safari/537.36")
 
-# for ChromeDriver version 79.0.3945.16 or over
-options.add_argument("--disable-blink-features=AutomationControlled")
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--ignore-ssl-errors')
-
-url = "https://www.lbank.com/login/"
+    # for ChromeDriver version 79.0.3945.16 or over
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--ignore-ssl-errors')
 
 caps = DesiredCapabilities().CHROME
 caps['pageLoadStrategy'] = 'eager'
@@ -31,69 +28,94 @@ service = Service(desired_capabilities=caps, executable_path=r"C:\drivers\chrome
 driver = webdriver.Chrome(service=service, options=options)
 
 try:
-    driver.maximize_window    
-    driver.get(url)
-    time.sleep(5)
-    
-    # print("Passing authentication...")
-    email_input = driver.find_element(By.XPATH, "//input[@placeholder='Please enter your email']")
-    email_input.clear()
-    email_input.send_keys(bank_emale)
 
-    password_input = driver.find_element(By.XPATH, "//input[@placeholder='Please enter password']")
-    password_input.clear()
-    password_input.send_keys(bank_password)
-    password_input.send_keys(Keys.ENTER)    
+    # Passing authentication...
+    def authentication(driver:webdriver.Chrome):
+        email_input = driver.find_element(By.XPATH, "//input[@placeholder='Please enter your email']")
+        email_input.clear()
+        email_input.send_keys(bank_emale)
+
+        password_input = driver.find_element(By.XPATH, "//input[@placeholder='Please enter password']")
+        password_input.clear()
+        password_input.send_keys(bank_password)
+        password_input.send_keys(Keys.ENTER)    
+
+    # установить значение amount
+    def set_amount(driver:webdriver.Chrome, arg:str, val:str):
+        InputForm = driver.find_element(By.XPATH, f"//span[contains(text(), '{arg}')]")
+        input = InputForm.find_element(By.XPATH, "./parent::div//input")
+        input.clear()
+        input.send_keys(val)
+
+    # установка слайдера в максимальное значение
+    def turn_trade_slider(driver:webdriver.Chrome, arg:str): 
+        slider = driver.find_element(By.XPATH, f"//div[contains(@class, '{arg}')]//div[@class='ant-slider-handle']")        
+        driver.execute_script("arguments[0].style.left = '100%'", slider)
+        driver.execute_script("arguments[0].style.transform = 'translateX(-50%)'", slider)
+        driver.execute_script("arguments[0].setAttribute('aria-valuenow', '100')", slider)
+
+        # проценты
+        percentage_element = slider.find_element(By.XPATH, "//span[contains(@style, 'left: 100%')]")
+        driver.execute_script("arguments[0].style.left = '100%'", percentage_element)
+        driver.execute_script("arguments[0].innerText = '100%'", percentage_element)
+        
+        slider_track = slider.find_element(By.XPATH, "//div[@class='ant-slider-track']")
+        driver.execute_script("arguments[0].style.width = '100%'", slider_track)
+
+    # нажать кнопку buy/sell
+    def click_trade_button(driver:webdriver.Chrome, arg:str):
+        button = driver.find_element(By.XPATH, f"//button[contains(@class, '{arg}')]")
+        driver.execute_script("arguments[0].click();", button)
+
+    # нажать кнопку cancel/confirm Order
+    # передаются два аргумента args {Cancel} / {Confirm}
+    def click_trade_confirm_button(driver:webdriver.Chrome, arg:str):
+        span = driver.find_element(By.XPATH, f"//span[contains(text(), '{arg}')]")
+        button = span.find_element(By.XPATH, "./parent::button")
+        driver.execute_script("arguments[0].click();", button)
+
+    # активировать чек-бокс "Don't prompt again"
+    def click_dont_prompt_again(driver:webdriver.Chrome):
+        try:
+            span = driver.find_element(By.XPATH, "//span[contains(text(), 'prompt again')]")
+            check = span.find_element(By.XPATH, "./parent::div")
+            driver.execute_script("arguments[0].click();", check)
+        except NoSuchElementException:
+            # Обработка случая, когда элемент не найден
+            print("Чек-бокс не найден")
+
+    # закрыть всплывающее диалоговое окно
+    def close_dialog_window(driver:webdriver.Chrome):
+        try:
+            dialog = driver.find_element(By.CLASS_NAME, "ant-modal-content")
+            closeButton = dialog.find_element(By.XPATH, "//button[contains(@aria-label, 'Close')]")
+            driver.execute_script("arguments[0].click();", closeButton)
+        except NoSuchElementException:
+            # Обработка случая, когда элемент не найден
+            print("Всплывающее диалоговое окно не найдено")           
+
+    driver.maximize_window
+    driver.get("https://www.lbank.com/login/")
     
+    time.sleep(5)
+    authentication(driver)
     time.sleep(15)
-    
     driver.get("https://www.lbank.com/en-US/trade/btc_usdt/")
-    
-    time.sleep(5)
+    time.sleep(3)
 
-    buyInputForm = driver.find_element(By.XPATH, "//span[contains(text(), 'Buying amount')]")
-    inputBuy = buyInputForm.find_element(By.XPATH, "./parent::div//input")
-    inputBuy.clear()
-    inputBuy.send_keys("5")
-        
-    tradeSliderGreen = driver.find_element(By.XPATH, "//div[contains(@class, 'tradeSliderGreen')]")
-    buySlider = tradeSliderGreen.find_element(By.CLASS_NAME, "ant-slider-handle")
+    turn_trade_slider(driver, "tradeSliderGreen")
+    set_amount(driver, "Buying amount", "5")        
+    click_trade_button(driver, "index_buy")
+    # click_trade_confirm_button(driver, "Confirm")
+    click_dont_prompt_again(driver)
+    close_dialog_window(driver)
     
-    driver.execute_script("arguments[0].style.left = '100%'", buySlider)
-    driver.execute_script("arguments[0].setAttribute('aria-valuenow', '100')", buySlider)
-        
-    byButton = driver.find_element(By.XPATH, "//button[contains(@class, 'index_buy')]")
-    driver.execute_script("arguments[0].click();", byButton)
-    
-    # всплывающее окно и его закрытие
-    dialog = driver.find_element(By.CLASS_NAME, "ant-modal-content")
-    closeButton = dialog.find_element(By.XPATH, "//button[contains(@aria-label, 'Close')]")
-    driver.execute_script("arguments[0].click();", closeButton)
-
-    # time.sleep(2)
-    
-    sellInputForm = driver.find_element(By.XPATH, "//span[contains(text(), 'Selling amount')]")
-    inputSell = sellInputForm.find_element(By.XPATH, "./parent::div//input")
-    inputSell.clear()
-    inputSell.send_keys("3")
-        
-    tradeSliderRed = driver.find_element(By.XPATH, "//div[contains(@class, 'tradeSliderRed')]")
-    sellSlider = tradeSliderRed.find_element(By.CLASS_NAME, "ant-slider-handle")
-
-    driver.execute_script("arguments[0].style.left = '100%'", sellSlider)
-    driver.execute_script("arguments[0].setAttribute('aria-valuenow', '100')", sellSlider)
-
-    selButton = driver.find_element(By.XPATH, "//button[contains(@class, 'index_sel')]")
-    driver.execute_script("arguments[0].click();", selButton)
-    
-    # time.sleep(2)
-    
-    # всплывающее окно и его закрытие
-    dialog = driver.find_element(By.CLASS_NAME, "ant-modal-content")
-    closeButton = dialog.find_element(By.XPATH, "//button[contains(@aria-label, 'Close')]")
-    driver.execute_script("arguments[0].click();", closeButton)
-
-    time.sleep(5)
+    turn_trade_slider(driver, "tradeSliderRed")
+    set_amount(driver, "Selling amount", "3")
+    click_trade_button(driver, "index_sel")
+    click_dont_prompt_again(driver)
+    # click_trade_confirm_button(driver, "Cancel")
+    close_dialog_window(driver)
     
 except Exception as ex:
     print(ex)
